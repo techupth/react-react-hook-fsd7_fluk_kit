081f128767df9160e5e21a8bc8457c66edaedc6a
Kittisak Saengsri <play_boy_kit@hotmail.com>
2024-10-31 14:02:16 +0700

    feat: added hook uesBlogPosts at HomePage & ViewPostPage

--- client/src/pages/ViewPostPage.jsx
+++ client/src/pages/ViewPostPage.jsx
@@ -1,29 +1,9 @@
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import useBlogPosts from "../components/hook/useBlogPosts";
 
 function ViewPostPage() {
   const navigate = useNavigate();
-
-  const [posts, setPosts] = useState([]);
-  const [isError, setIsError] = useState(null);
-  const [isLoading, setIsLoading] = useState(null);
-
-  const getPosts = async () => {
-    try {
-      setIsError(false);
-      setIsLoading(true);
-      const results = await axios("http://localhost:4000/posts");
-      setPosts(results.data.data);
-      setIsLoading(false);
-    } catch (error) {
-      setIsError(true);
-    }
-  };
-
-  useEffect(() => {
-    getPosts();
-  }, []);
+  const { posts, isError, isLoading } = useBlogPosts();
 
   return (
     <div>
@@ -42,6 +22,7 @@ function ViewPostPage() {
               <h1>{post.title}</h1>
               <div className="post-actions">
                 <button className="view-button">View post</button>
+                <button className="edit-button">Edit post</button>
               </div>
             </div>
           );
